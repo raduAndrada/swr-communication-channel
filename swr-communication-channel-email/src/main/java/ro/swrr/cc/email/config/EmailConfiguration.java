04f3dbf1065d3e3787f1868b57f447172f687999
andrada radu <eozlxo3ng84rptcl0q0vlnfhs3qfyv@bots.bitbucket.org>
2024-11-06 18:17:17 +0200

    config: removed locale

--- swr-communication-channel-email/src/main/java/ro/swrr/cc/email/config/EmailConfiguration.java
+++ swr-communication-channel-email/src/main/java/ro/swrr/cc/email/config/EmailConfiguration.java
@@ -4,9 +4,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.servlet.LocaleResolver;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 import org.thymeleaf.spring5.SpringTemplateEngine;
@@ -17,8 +14,7 @@ import java.util.Locale;
 
 
 @Configuration
-@EnableWebMvc
-public class EmailConfiguration implements WebMvcConfigurer {
+public class EmailConfiguration {
 
 //    @Bean
 //    public CommandLineRunner dataLoader(EmailService emailService)  {
@@ -50,7 +46,8 @@ public class EmailConfiguration implements WebMvcConfigurer {
     public ResourceBundleMessageSource emailMessageSource() {
         ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
         messageSource.setBasename("mailMessages");
-        return messageSource;   }
+        return messageSource;
+    }
 
     @Bean
     public LocaleResolver localeResolver() {
@@ -64,11 +61,6 @@ public class EmailConfiguration implements WebMvcConfigurer {
         LocaleChangeInterceptor lci = new LocaleChangeInterceptor();
         lci.setParamName("lang");
         return lci;
-    }
-
-    @Override
-    public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(localeChangeInterceptor());
     }
 
 
